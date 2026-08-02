7d82b1966ebe21d39c206c7ec07a3a787e0491f5
Julien FAURE <julien.y.faure@gmail.com>
2026-08-02 22:07:19 +0200

    séparation code et naration

---
+++ contenus/template.ts
@@ -0,0 +1,19 @@
+const passage={
+    id: "Template",
+    title: "Le template",
+    texte: ["J'aime les pommes"],
+    illustration: null, 
+    ambiance: "grosse ambiance",
+    choix: [
+        {
+            texte: "choix 1",
+            destination: "Template X"
+        },
+        {
+            texte: "choix 2",
+            destination: "Template Y"
+        }
+    ]
+};
+
+export default passage;
