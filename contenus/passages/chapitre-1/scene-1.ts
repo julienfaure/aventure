60d87fa5d4c7d96b2779fd28f763146c3c2ccd52
Julien FAURE <julien.y.faure@gmail.com>
2026-08-04 22:41:49 +0200

    séparation introduction en 2

--- contenus/passages/chapitre-1/scene-1.ts
+++ contenus/passages/chapitre-1/scene-1.ts
@@ -1,17 +1,30 @@
 const passage={
     id: "scene-1",
-    number: "170",
-    resume: `scène 1`,
-    texte: [`texte de la scène 1, ce texte est vraiment très intéressant, on y apprend beaucoup de choses.`,],
+    number: "1",
+    resume: `Marqueurs d'abandon des lieux. Premier choix du héros.`,   
+    texte: [`Depuis l'ombre d'un des platanes qui bordent la place de l'église, vous observez la rue commerçante du village.
+    Il n'y a que vos traces de pas dans l'épaisse couche de poussière crayeuse blanche qui recouvre aussi vos bottes.
+    Le ciel dégagé vous permet d'apercevoir le `, 
+    {
+        wiki: "pic-du-nid-du-monde",
+        texte: "Pic du Nid du Monde",
+    },
+    `, encore enneigé, 
+    s'élevant au dessus des versants vordoyants.
+    `],
     illustration: null, 
-    choix: [
+     choix: [
         {
-            texte: `choix 1`,
-            destination: "scene-2"
+            texte: `Se diriger vers le parvis de l'église (qui est aussi la place du village).`,
+            destination: "PlaceCentrale"
         },
         {
-            texte: `choix 2`,
-            destination: "scene-3"
+            texte:`Descendre la rue vers la taverne`,
+            destination: 'Boulangerie'
+        },
+        {
+            texte:`Monter la rue vers le maréchal ferrand.`,
+            destination: 'Boucherie'
         }
     ]
 };
--- contenus/passages/introduction.ts
+++ contenus/passages/introduction.ts
@@ -1,19 +1,8 @@
 const passage={
     id: "introduction",
-    number: "1",
+    number: "je veux vivre une aventure",
     resume: `Le héros demande au compagnon de lui faire vivre une aventure.`,   
-    texte: [`Depuis l'ombre d'un des platanes qui bordent la place de l'église, vous observez la rue commerçante du village.
-    Il n'y a que vos traces de pas dans l'épaisse couche de poussière crayeuse blanche qui recouvre aussi vos bottes.
-    Le ciel dégagé vous permet d'apercevoir le `, 
-    {
-        wiki: "pic-du-nid-du-monde",
-        texte: "Pic du Nid du Monde",
-    },
-    `, encore enneigé, 
-    s'élevant au dessus des versants vordoyants.
-    Vous pestez contre le bruit lointain de la mine qui vous empêche de dormir, de jour comme de nuit.
-    Vous reconnaissez toutefois que votre agacement trahit surtout votre ennui profond. Vous vous tournez vers votre compagnon:
-    `],
+    texte: [``],
     illustration: null, 
      choix: [
         {
